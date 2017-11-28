b00524412411351bb0668b5f5775e3364e9daae2
ccs1910 <christian.cahyadi@gmail.com>
2017-11-28 16:02:54 +0700

    new URL camera off camera on

--- app.py
+++ app.py
@@ -99,13 +99,13 @@ def processRequest(req):
         text = req['result']['parameters'].get('text')
         
         if text == "turn on" or text == "open":
-            baseurl = "https://d39493c8.ngrok.io/camera_turn_on"
+            baseurl = "https://cb7f63be.ngrok.io/camera_turn_on"
             content = urlopen(baseurl).read()
             speech = "See your Raspberry camera is ON"
             res = camera_response(speech)
             
         elif text == "turn off" or text == "close":
-            baseurl = "https://d39493c8.ngrok.io/camera_turn_off"
+            baseurl = "https://cb7f63be.ngrok.io/camera_turn_off"
             content = urlopen(baseurl).read()
             speech = "See your Raspberry camera is OFF"
             res = camera_response(speech)
